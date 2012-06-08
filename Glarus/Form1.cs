96c112f881efba067f2143d43d31c2a15cfc28ea
MAXX <mba_doc@mail.ru>
2012-06-08 18:12:31 +0400

    1

--- Glarus/Form1.cs
+++ Glarus/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace WindowsFormsApplication1
 {
@@ -14,6 +15,36 @@ namespace WindowsFormsApplication1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                //CREATE SCHEMA `GLARUS_DB` ;
+                string server;
+                string database;
+                string uid;
+                string password;
+
+                server = "localhost";
+                database = "GLARUS_DB";
+                uid = "root";
+                password = "RjvgfybzRBN25";
+                string connectionString;
+                connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+                database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+                MySqlConnection myConnection = new MySqlConnection(connectionString);
+                myConnection.Open();
+                myConnection.Close();
+                MessageBox.Show("Подключение прошло успешно!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.Message);
+            }
+
         }
     }
 }
