88c0884767962001d994cf50f8ca3384745d4390
MAXXL <mba_doc@mail.ru>
2012-06-12 16:16:16 +0400

    pacient
    db

--- Glarus/MySQL/Dump20120609.sql
+++ Glarus/MySQL/Dump20120609.sql
@@ -31,7 +31,7 @@ CREATE TABLE `user` (
   `SecondName` varchar(25) DEFAULT NULL,
   `Password` varchar(25) DEFAULT NULL,
   PRIMARY KEY (`idUser`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,7 +40,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'Иванов','Иван','Иванович','1'),(2,'Петров','Петр','Петрович','2'),(3,'Сидоров','Сидор','Сидорович','3');
+INSERT INTO `user` VALUES (1,'Иванов','Иван','Иванович','1'),(2,'Петров','Петр','Петрович','2'),(3,'Сидоров','Сидор','Сидорович','3'),(4,'Николаев','Николай','Николаевич','4');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -58,7 +58,7 @@ CREATE TABLE `pacient` (
   `SecondName` varchar(25) DEFAULT NULL,
   `Birthday` date DEFAULT NULL,
   PRIMARY KEY (`idPacient`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -67,6 +67,7 @@ CREATE TABLE `pacient` (
 
 LOCK TABLES `pacient` WRITE;
 /*!40000 ALTER TABLE `pacient` DISABLE KEYS */;
+INSERT INTO `pacient` VALUES (4,'Агапкова','Марьяна','Федоровна','1970-06-10'),(5,'Пилипко','Татьяна','Сергеевна','1964-04-01'),(6,'Прохорова','Инна','Федоровна','1980-06-10');
 /*!40000 ALTER TABLE `pacient` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -79,4 +80,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2012-06-09 17:55:49
+-- Dump completed on 2012-06-12 16:15:40
